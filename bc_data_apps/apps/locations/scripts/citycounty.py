6ee295178b5883560747793a550342a55d893a32
Shane Shifflett <shifflett.shane@gmail.com>
2012-09-11 17:17:08 -0700

    refactor for namespace

--- bc_data_apps/apps/locations/scripts/citycounty.py
+++
@@ -1,8 +0,0 @@
-from bc_data_apps.apps.locations.models import Coordinates
-
-
-def run():
-    for c in Coordinates.objects.all():
-        if c.city and c.county:
-            c.city.county = c.county
-            c.city.save()
