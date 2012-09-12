50a108a9792eedc4bdb5f7fbc23663e9994769d2
Shane Shifflett <shifflett.shane@gmail.com>
2012-09-11 19:10:15 -0700

    dj the right db url

--- data_apps/settings.py
+++ data_apps/settings.py
@@ -27,6 +27,9 @@ DATABASES = {
         'HOST':'localhost',
     },
 }
+if not DEBUG:
+    dburl = env('DATABASE_URL', '')
+    DATABASES = {'default': dj_database_url.config(default=dburl)}
 
 # Local time zone for this installation. Choices can be found here:
 # http://en.wikipedia.org/wiki/List_of_tz_zones_by_name
