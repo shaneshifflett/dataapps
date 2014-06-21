013e5464377a494b8dd0cd96b250f1b3052ed177
Shane Shifflett <shifflett.shane@gmail.com>
2014-06-21 13:56:50 -0400

    update path to wsgi

--- data_apps/wsgi.py
+++ data_apps/wsgi.py
@@ -15,7 +15,7 @@ framework.
 """
 import os
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "bc_data_apps.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "data_apps/bc_data_apps.settings")
 
 # This application object is used by any WSGI server configured to use this
 # file. This includes Django's development server, if the WSGI_APPLICATION
